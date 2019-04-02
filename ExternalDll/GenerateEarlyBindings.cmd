1268c12059ef1d32300c78fd8e24fba79a7ab7ad
Hill, Roger <roger.hill@debeersgroup.com>
2019-04-02 10:10:06 +0100

    Prune legacy directories, update to latest .Net and nuget versions. Build check. Check test suite.

--- ExternalDll/GenerateEarlyBindings.cmd
+++
@@ -1,6 +0,0 @@
-﻿REM
-REM Use CRMSVCUtil to regenerate the Early Bindings.
-..\coretools\crmsvcutil.exe /url:https://rbh3.api.crm11.dynamics.com/XRMServices/2011/Organization.svc /out:Entities.cs ^
-   /username:roger@rbh3.onmicrosoft.com /password:Password123 ^
-   /namespace:DynamicsLinq /serviceContextName:OrgServiceContext
-	
